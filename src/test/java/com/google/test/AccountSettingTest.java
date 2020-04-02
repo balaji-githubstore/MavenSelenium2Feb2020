884a9019c91d2a16be26fca808f5d17707af555f
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2020-04-02 08:22:04 +0530

    third commit

---
+++ src/test/java/com/google/test/AccountSettingTest.java
@@ -0,0 +1,13 @@
+package com.google.test;
+
+import org.testng.annotations.Test;
+
+public class AccountSettingTest {
+	
+	@Test
+	public void newTest()
+	{
+		System.out.println("Test mode");
+	}
+
+}
